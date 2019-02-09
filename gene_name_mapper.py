d66e7e95ebab3189523497fd9a212d3d4ad53fad
Yue Zhang <yjzhang@cs.washington.edu>
2019-02-08 19:37:03 -0800

    gene name mapper

--- gene_name_mapper.py
+++ gene_name_mapper.py
@@ -1,7 +1,30 @@
 import sqlite3
 
-def get_gene_symbol(accession):
+DB_DIR = 'data/genes.db'
+
+def get_gene_symbol(accessions):
     """
-    Given a string that is either a RefSeq ID ('NM_...') or an
-    Ensembl ID ('ENSG...'), this returns the standard gene symbol.
+    Given a string or list of strings that are either RefSeq ID ('NM_...')
+    or Ensembl ID ('ENSG...'), this returns a list of gene symbols.
     """
+    conn = sqlite3.connect(DB_DIR)
+    c = conn.cursor()
+    if isinstance(accessions, str):
+        accessions = [accessions]
+    all_results = []
+    for a in accessions:
+        c.execute('SELECT "Approved symbol" FROM gene_names_homo_sapiens WHERE "RefSeq IDs"=? OR "Ensembl gene ID"=?', (a, a))
+        results = c.fetchall()
+        if len(results) == 1:
+            all_results.append(results[0][0])
+        elif len(results) == 0:
+            print('WARNING: no results found for ' + a)
+            all_results.append(None)
+        else:
+            print('WARNING: multiple results found for ' + a + '. Using arbitrary result.')
+            all_results.append(results[0][0])
+    return all_results
+
+if __name__ == '__main__':
+    print(get_gene_symbol('NM_001178126'))
+    print(get_gene_symbol(['NM_001178126', 'NM_001136213']))
